14a6e6b1106c27de1acab85c60a9a97337c1729b
Eric Yarham <eric.yarham@gmail.com>
2021-09-04 16:53:59 -0400

    make it right

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Literally Eric
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
